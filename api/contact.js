a6c92a8b9f5d970525d3c94bf0eef23f28d35c0f
Mayke-Agency <alan@maykeagency.com>
2026-03-29 17:08:27 -0400

    form update

--- api/contact.js
+++ api/contact.js
@@ -3,10 +3,10 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed." });
   }
 
-  const { firstName, lastName, email, message, company, formStarted } =
+  const { firstName, lastName, email, message, websiteUrl, formStarted } =
     req.body || {};
 
-  if (company) {
+  if (websiteUrl) {
     return res.status(400).json({ error: "Spam detected." });
   }
 
