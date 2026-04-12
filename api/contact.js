1ddc2ca075bf9b4338d865ad4b41401a52cb511c
Mayke-Agency <alan@maykeagency.com>
2026-04-12 10:19:10 -0400

    Update contact.js

--- api/contact.js
+++ api/contact.js
@@ -46,7 +46,7 @@ export default async function handler(req, res) {
   try {
     await resend.emails.send({
       from: "Jeté Dance Center <onboarding@resend.dev>",
-      to: ["jetedancetx@gmail.com"], // ← change if needed
+      to: ["alan.campos.arroyo@gmail.com"], // ← change if needed
       subject: "New Contact Form Submission",
       reply_to: email,
       text: `
